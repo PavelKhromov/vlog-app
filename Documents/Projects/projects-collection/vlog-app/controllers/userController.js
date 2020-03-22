448e1d235334068c049a6fc787f773475c12e706
Pavel <p.v.hromov@gmail.com>
2020-03-21 20:35:24 -0400

    Firs Commit1

---
+++ Documents/Projects/projects-collection/vlog-app/controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = function() {
+
+}
+
+exports.logout = function() {
+    
+} 
+
+exports.register = function() {
+    
+} 
+
+exports.home = function(res, res) {
+    res.render('home-guest')
+} 
